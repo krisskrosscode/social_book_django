f77714433e929e3dd50c3e01f4b7b217826de074
krisskrosscode <harshalparteke@gmail.com>
2023-02-26 16:24:51 +0530

    list filters added

--- static/file_upload.js
+++ static/file_upload.js
@@ -1,81 +1,28 @@
-
-$("#upload_form").submit(function (e) {
-  // preventing from page reload and default actions
-  e.preventDefault();
-  // serialize the data for sending the form data.
-  // var serializedData = $(this).serialize();
-  var form_data = new FormData();
-
-      for (var x = 0; x < ins; x++) {
-        form_data.append(
-          "files[]",
-          document.getElementById("multiFiles").files[x]
-        );
-        console.log("file uploading");
-  // make POST ajax call
+function upload() {
+  console.log("Uploaded");
+  var formData = new FormData($("#upload_form")[0]);
+  console.log(formData);
   $.ajax({
-    type: "POST",
     url: "{% url 'view' %}",
-    data: serializedData,
-    success: function (response) {
-      // on successfull creating object
-      // 1. clear the form.
-      $("#upload_form").trigger("reset");
-      console.log("success");
+    type: "POST",
+    data: formData,
+    success: function (data) {
+      $(".error").remove();
+      //console.log(data);
+
+      if (data.error) {
+        alert(data.error);
+      } else {
+        console.log("Success");
+        window.alert("Your File is uploaded");
+        window.location.href = "{% url 'view' %}";
+      }
     },
-    error: function (response) {
-      // alert the error if any error occured
-      alert("Error in upload");
+    error: function (data) {
+      console.log("Error occured");
     },
+    cache: false,
+    contentType: false,
+    processData: false,
   });
-});
-
-
-
-
-// $(document).ready(function (e) {
-//   $("#upload").on("click", function () {
-//     var form_data = new FormData();
-//     var ins = document.getElementById("multiFiles").files.length;
-
-//     if (ins == 0) {
-//       $("#msg").html('<span style="color:red">Select at least one file</span>');
-//       console.log("No files uploaded");
-//       return;
-//     }
-
-//     for (var x = 0; x < ins; x++) {
-//       form_data.append(
-//         "files[]",
-//         document.getElementById("multiFiles").files[x]
-//       );
-//       console.log("file uploading");
-//     }
-
-//     csrf_token = $('input[name="csrfmiddlewaretoken"]').val();
-
-//     //console.log(csrf_token);
-
-//     form_data.append("csrfmiddlewaretoken", csrf_token);
-
-//     $.ajax({
-//       url: "upload", // point to server-side URL
-//       dataType: "json", // what to expect back from server
-//       cache: false,
-//       contentType: false,
-//       processData: false,
-//       //data: {'data': form_data, 'csrfmiddlewaretoken': csrf_token},
-//       data: form_data,
-//       type: "post",
-//       success: function (response) {
-//         // display success response
-//         $("#msg").html(response.msg);
-//         console.log("SUCessfully uploaded");
-//       },
-//       error: function (response) {
-//         $("#msg").html(response.message); // display error response
-//         console.log("Error in uploading");
-//       },
-//     });
-//   });
-// });
+}
